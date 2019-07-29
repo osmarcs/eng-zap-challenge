0410a5e2c3a9fb0582dabbc3e00a2cd85bd4df95
Osmar CS <osmar.costa.s@gmail.com>
2019-07-29 20:55:32 -0300

    :sparkles: recuperando dados da api e criado as rotas para as paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import './styles.css';
+import Search from './pages/Search';
+import getAllProperties from './services/getProperties';
+import Header from './compositions/Header';
+import Details from './pages/Details';
 
 function App() {
+  const [properties, setProperties] = useState([]);
+
+  async function fetchData() {
+    const data = await getAllProperties();
+    setProperties(data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  function filterProperties(type) {
+    const field = type === 'zap' ? 'isAvaliableZAP' : 'isAvaliableVivaReal';
+    return properties.filter(item => item[field]);
+  }
+
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route
+          path="/zap"
+          component={() => <Search properties={filterProperties('zap')} />}
+        />
+        <Route
+          path="/vivareal"
+          component={() => <Search properties={filterProperties()} />}
+        />
+        <Route
+          path="/details/:id"
+          component={() => <Details properties={properties} />}
+        />
+      </div>
+    </Router>
   );
 }
 
